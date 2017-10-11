4486cdcfdca73e970f5be77b2813c4a12d011ab9
SyafiqTermizi <ahmadsyafiq93@gmail.com>
2017-10-11 22:33:10 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='solat',
+    description='Malaysia prayer time based on jakim XML',
+    author='syafiq',
+    packages=['solat'],
+    zip_safe=False
+)
---
+++ solat/__init__.py
@@ -0,0 +1,27 @@
+from urllib.request import urlopen
+import xmltodict
+
+def solat(district):
+
+    base_url = 'http://www2.e-solat.gov.my/xml/today/?zon='
+    final_url = base_url + district
+
+    jakim_xml = xmltodict.parse(urlopen(final_url).read())
+
+    solat.districts = jakim_xml['rss']['channel']['description']
+    solat.curr_time = jakim_xml['rss']['channel']['dc:date']
+
+    solat.prayers = []
+    solat.pray_time = []
+
+    counter = 0
+    while(counter < 7):
+        solat.prayers.append(
+                        jakim_xml['rss']['channel']['item'][counter]['title']
+                        )
+        solat.pray_time.append(
+                            jakim_xml['rss']['channel']['item'][counter]['description']
+                            )
+        counter += 1
+
+    solat.prayers_compact = zip(solat.prayers, solat.pray_time)
